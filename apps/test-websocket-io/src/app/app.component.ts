b90328bbc670943f2fa508cbf663b590215c0fdf
jzhu077 <zhujunyu168@hotmail.com>
2019-11-13 20:34:53 +1300

    Set up a simple chatting service

--- apps/test-websocket-io/src/app/app.component.ts
+++ apps/test-websocket-io/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@ws-test/api-interfaces';
+import { ChatService } from './services/chat.service';
 
 @Component({
   selector: 'ws-test-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+export class AppComponent implements OnInit{
+  public users = 0;
+  public message = '';
+  public messages: string[] = [];
+
+  constructor(private chatService: ChatService){
+
+  }
+
+  ngOnInit(){
+
+    this.chatService.receiveChat().subscribe((message: string) => {
+      this.messages.push(message);
+    });
+
+    this.chatService.getUsers().subscribe((users: number) => {
+      this.users = users;
+    });
+
+  }
+
+  addChat(){
+    this.messages.push(this.message);
+    this.chatService.sendChat(this.message);
+    this.message = '';
+  }
 }
--- apps/test-websocket-io/src/app/app.module.ts
+++ apps/test-websocket-io/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { FormsModule } from '@angular/forms';
+
+const config: SocketIoConfig = { url: 'http://localhost:3333', options: {} };
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, SocketIoModule.forRoot(config), FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
